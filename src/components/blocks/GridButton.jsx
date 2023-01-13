7f5d59a5a8b98b90ad225a8c89da79b14724c6e5
Tim Krause <timkrausedev@gmail.com>
2023-01-13 17:35:49 -0500

    update handleclick button to fix starting point.

--- src/components/blocks/GridButton.jsx
+++ src/components/blocks/GridButton.jsx
@@ -1,53 +1,56 @@
 import { useRef } from 'react';
 
-const GridButton = ({ 
-	children, 
-	version,
-	title,
-	setScaleX, 
-	setScaleY, 
-	setTranslateX, 
-	setTranslateY, 
-	popupRef, 
-	setIsActive,
-	setActiveVersion,
-	setActiveTitle,
-	isPopup
+const GridButton = ({
+    children,
+    version,
+    title,
+    setScaleX,
+    setScaleY,
+    setTranslateX,
+    setTranslateY,
+    popupRef,
+    setIsActive,
+    setActiveVersion,
+    setActiveTitle,
+    isPopup
 }) => {
-	const buttonRef = useRef(null);
+    const buttonRef = useRef(null);
 
     const handleButtonClick = () => {
 
-    	setActiveTitle(title || '');
-    	setActiveVersion(version || 1);
+        setActiveTitle(title || '');
+        setActiveVersion(version || 1);
+
+        const buttonPosition = buttonRef.current.getBoundingClientRect();
 
         setScaleX(buttonRef.current.offsetWidth / popupRef.current.offsetWidth);
         setScaleY(buttonRef.current.offsetHeight / popupRef.current.offsetHeight);
-        setTranslateX(buttonRef.current.offsetLeft + 'px');
-        setTranslateY(buttonRef.current.offsetTop + 'px');
+        setTranslateX(`${Math.floor(buttonPosition.left)}px`);
+        setTranslateY(`${Math.floor(buttonPosition.top)}px`);
+
         setActiveVersion(version);
 
         setTimeout(() => {
-	        setIsActive(true);
-	        setTranslateX(0)
-	        setTranslateY(0)
-	        setScaleX(1)
-	        setScaleY(1)
+            setIsActive(true);
+            setTranslateX(0)
+            setTranslateY(0)
+            setScaleX(1)
+            setScaleY(1)
         }, 100)
     }
 
-    if(!isPopup) {
-    	return <div className={`grid-button grid-button--${version}`} >{children}</div>
+    if (!isPopup) {
+        return <div className={`grid-button grid-button--${version}`} >{children}</div>
     }
 
-	return (
-		<button 
-			ref={buttonRef} 
-			className={`grid-button grid-button--${version}`} 
-			onClick={handleButtonClick}
-		>{children}</button>
-	
-	);
+    return (
+        <button
+            ref={buttonRef}
+            className={`grid-button grid-button--${version}`}
+            onClick={handleButtonClick}
+        >{children}</button>
+
+    );
 }
 
 export default GridButton;
